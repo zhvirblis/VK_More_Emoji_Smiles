c461ea5fb45ae2e17dbd489d815eba71fb6dd247
zhvirblis <yurocker@mail.ru>
2016-12-04 17:27:48 +0300

    Version 0.2.1

---
+++ content.js
@@ -0,0 +1,181 @@
+var codes =["003020E3","003120E3","003220E3","003320E3","003420E3","003520E3","003620E3","003720E3","003820E3","003920E3","203C","2049","2139","2194","2195","2196","2197","2198","2199","21A9","21AA","231A","231B","23E9","23EA","23EB","23EC","24C2","25AA","25AB","25B6","25C0","25FB","25FC","25FD","25FE","2611","2614","2648","2649","264A","264B","264C","264D","264E","264F","2650","2651","2652","2653","2660","2663","2665","2666","2668","267F","2693","26AA","26AB","26CE","26F2","26FA","2705","2714","2716","2733","2734","2747","274C","274E","2753","2754","2755","2757","2795","2796","2797","27A1","27B0","27BF","2934","2935","2B05","2B06","2B07","2B1B","2B1C","2B50","2B55","3030","303D","D83CDD70","D83CDD71","D83CDD7E","D83CDD7F","D83CDD8E","D83CDD91","D83CDD92","D83CDD93","D83CDD94","D83CDD95","D83CDD96","D83CDD97","D83CDD99","D83CDD9A","D83CDDE6D83CDDEA","D83CDDE6D83CDDF9","D83CDDE6D83CDDFA","D83CDDE7D83CDDEA","D83CDDE7D83CDDF7","D83CDDE8D83CDDE6","D83CDDE8D83CDDED","D83CDDE8D83CDDF1","D83CDDE8D83CDDF4","D83CDDE9D83CDDF0","D83CDDEBD83CDDEE","D83CDDEDD83CDDF0","D83CDDEED83CDDE9","D83CDDEED83CDDEA","D83CDDEED83CDDF3","D83CDDF2D83CDDF4","D83CDDF2D83CDDFD","D83CDDF2D83CDDFE","D83CDDF3D83CDDF1","D83CDDF3D83CDDF4","D83CDDF3D83CDDFF","D83CDDF5D83CDDED","D83CDDF5D83CDDF1","D83CDDF5D83CDDF7","D83CDDF5D83CDDF9","D83CDDF8D83CDDE6","D83CDDF8D83CDDEA","D83CDDF8D83CDDEC","D83CDDFBD83CDDF3","D83CDDFFD83CDDE6","D83CDE01","D83CDF00","D83CDF01","D83CDF03","D83CDF04","D83CDF05","D83CDF06","D83CDF07","D83CDF08","D83CDF09","D83CDF0A","D83CDF0B","D83CDF0C","D83CDF0E","D83CDF0F","D83CDF10","D83CDF11","D83CDF12","D83CDF13","D83CDF14","D83CDF15","D83CDF16","D83CDF17","D83CDF18","D83CDF19","D83CDF1A","D83CDF1C","D83CDF20","D83CDF58","D83CDF59","D83CDF86","D83CDF87","D83CDF91","D83CDFA0","D83CDFA1","D83CDFA2","D83CDFA5","D83CDFA6","D83CDFAE","D83CDFB5","D83CDFB6","D83CDFBC","D83CDFE0","D83CDFE1","D83CDFE2","D83CDFE3","D83CDFE4","D83CDFE5","D83CDFE7","D83CDFE8","D83CDFE9","D83CDFEA","D83CDFEB","D83CDFEC","D83CDFED","D83CDFEE","D83CDFEF","D83CDFF0","D83DDC49","D83DDC65","D83DDCA0","D83DDCA2","D83DDCA4","D83DDCAB","D83DDCAE","D83DDCAF","D83DDCB1","D83DDCB2","D83DDCB9","D83DDCC0","D83DDCC1","D83DDCC2","D83DDCC3","D83DDCC6","D83DDCCF","D83DDCDB","D83DDCDE","D83DDCE3","D83DDCE4","D83DDCE5","D83DDCE7","D83DDCE8","D83DDCE9","D83DDCEA","D83DDCEB","D83DDCEC","D83DDCF2","D83DDCF3","D83DDCF4","D83DDCF5","D83DDCF6","D83DDD00","D83DDD01","D83DDD02","D83DDD03","D83DDD04","D83DDD05","D83DDD07","D83DDD08","D83DDD09","D83DDD0A","D83DDD0B","D83DDD0C","D83DDD0D","D83DDD0F","D83DDD10","D83DDD12","D83DDD13","D83DDD15","D83DDD17","D83DDD18","D83DDD19","D83DDD1A","D83DDD1B","D83DDD1C","D83DDD1D","D83DDD1F","D83DDD20","D83DDD21","D83DDD22","D83DDD23","D83DDD24","D83DDD2F","D83DDD32","D83DDD33","D83DDD34","D83DDD35","D83DDD36","D83DDD37","D83DDD38","D83DDD39","D83DDD3A","D83DDD3B","D83DDD3C","D83DDD3D","D83DDDFB","D83DDDFC","D83DDDFD","D83DDDFE","D83DDE17","D83DDE4D","D83DDE89","D83DDE8B","D83DDEA2","D83DDEA5","D83DDEA6","D83DDEA9","D83DDEAB","D83DDEAD","D83DDEAE","D83DDEAF","D83DDEB0","D83DDEB1","D83DDEB2","D83DDEB3","D83DDEB7","D83DDEB8","D83DDEB9","D83DDEBA","D83DDEBB","D83DDEBC","D83DDEBE","D83DDEC1","D83DDEC2","D83DDEC3","D83DDEC4","D83DDEC5","D83CDDEED83CDDF1","D83CDDF9D83CDDF7","2328","23ED","23EE","23EF","23F1","23F2","23F8","23F9","23FA","2602","2603","2604","2618","2620","2622","2623","2626","262A","262E","262F","2638","2639","2692","2694","2696","2697","2699","269B","269C","26B0","26B1","26C8","26CF","26D1","26D3","26E9","26F0","26F1","26F4","26F7","26F8","26F9","270D","271D","2721","2763","D83CDF21","D83CDF24","D83CDF25","D83CDF26","D83CDF27","D83CDF28","D83CDF29","D83CDF2A","D83CDF2B","D83CDF2C","D83CDF2D","D83CDF2E","D83CDF2F","D83CDF36","D83CDF7D","D83CDF7E","D83CDF7F","D83CDF96","D83CDF97","D83CDF99","D83CDF9A","D83CDF9B","D83CDF9E","D83CDF9F","D83CDFC5","D83CDFCB","D83CDFCC","D83CDFCD","D83CDFCE","D83CDFCF","D83CDFD0","D83CDFD1","D83CDFD2","D83CDFD3","D83CDFD4","D83CDFD5","D83CDFD6","D83CDFD7","D83CDFD8","D83CDFD9","D83CDFDA","D83CDFDB","D83CDFDC","D83CDFDD","D83CDFDE","D83CDFDF","D83CDFF3","D83CDFF4","D83CDFF5","D83CDFF7","D83CDFF8","D83CDFF9","D83CDFFA","D83DDC3F","D83DDC41","D83DDCF8","D83DDCFD","D83DDCFF","D83DDD49","D83DDD4A","D83DDD4B","D83DDD4C","D83DDD4D","D83DDD4E","D83DDD6F","D83DDD70","D83DDD73","D83DDD74","D83DDD75","D83DDD76","D83DDD77","D83DDD78","D83DDD79","D83DDD87","D83DDD8A","D83DDD8B","D83DDD8C","D83DDD8D","D83DDD90","D83DDD96","D83DDDA5","D83DDDA8","D83DDDB1","D83DDDB2","D83DDDBC","D83DDDC2","D83DDDC3","D83DDDC4","D83DDDD1","D83DDDD2","D83DDDD3","D83DDDDC","D83DDDDD","D83DDDDE","D83DDDE1","D83DDDE3","D83DDDEF","D83DDDF3","D83DDDFA","D83DDE41","D83DDE42","D83DDE43","D83DDE44","D83DDECB","D83DDECC","D83DDECD","D83DDECE","D83DDECF","D83DDED0","D83DDEE0","D83DDEE1","D83DDEE2","D83DDEE3","D83DDEE4","D83DDEE5","D83DDEE9","D83DDEEB","D83DDEEC","D83DDEF0","D83DDEF3","D83EDD10","D83EDD11","D83EDD12","D83EDD13","D83EDD14","D83EDD15","D83EDD16","D83EDD17","D83EDD18","D83EDD80","D83EDD81","D83EDD82","D83EDD83","D83EDD84","D83EDDC0","D83DDDE8","D83DDC41D83DDDE8","3297","3299","D83CDE02","D83CDE1A","D83CDE2F","D83CDE32","D83CDE33","D83CDE34","D83CDE35","D83CDE36","D83CDE37","D83CDE38","D83CDE39","D83CDE3A","D83CDE50","D83CDE51","D83DDD50","D83DDD51","D83DDD52","D83DDD53","D83DDD54","D83DDD55","D83DDD56","D83DDD57","D83DDD58","D83DDD59","D83DDD5A","D83DDD5B","D83DDD5C","D83DDD5D","D83DDD5E","D83DDD5F","D83DDD60","D83DDD61","D83DDD62","D83DDD63","D83DDD64","D83DDD65","D83DDD66","D83DDD67","D83DDD30"];
+
+var x = document.getElementsByClassName("emoji_smile");
+for (var i = 0; i < x.length; i++) {
+    x[i].ondblclick = EmojiAddList;
+    
+}
+
+//var lastflag = false;
+//var on_add = false;
+
+var body = document.querySelector('body');
+var observer = new window.MutationObserver(function(mutations) {
+    mutations.forEach(function(mutation) {
+
+        if (mutation.type === 'childList') {
+            for (var i = 0; i < mutation.addedNodes.length; i++) 
+            {   if (mutation.addedNodes[i].nodeType != 1 && mutation.addedNodes[i].nodeType != 9) 
+                    continue;
+                
+
+                /*for(var j=0;j<mutation.addedNodes[i].classList.length;j++){
+                  if(mutation.addedNodes[i].classList[j]=="emoji_smile_cont"
+                    &&mutation.addedNodes[i].getAttribute('onmousedown')=="Emoji.addEmoji(Emoji.shownId, 'D83CDDE7D83CDDFE', this); return cancelEvent(event);"){
+                    if(on_add){
+                      EmojiAddList();
+                    }
+                    lastflag=true;
+                  }
+                }*/
+                
+                if (mutation.addedNodes[i].querySelectorAll(".emoji_smile").length > 0){
+                    for (var j = 0; j < x.length; j++) {
+                       x[j].ondblclick = EmojiAddList;
+                    }
+                }
+            }
+        }
+
+        var body = document.querySelector('body');
+        observer.observe(body, {
+            childList: true,
+            subtree: true
+        });
+    });
+});
+observer.observe(body, {
+    childList: true,
+    subtree: true
+});
+
+function EmojiAddList(){
+ var emoji_scroll = document.getElementsByClassName("emoji_scroll");
+
+  for (var i = 0; i < emoji_scroll.length; i++) {
+
+  var emoji_cat_title_helper = document.createElement("div");
+  emoji_cat_title_helper.setAttribute("class", "emoji_cat_title_helper emoji_cat_title_fix emmegapuck");
+  var emoji_cat_title = document.createElement("div");
+  emoji_cat_title.setAttribute("class", "emoji_cat_title");
+
+  emoji_cat_title.innerHTML = '<b>Дополнительные смайлы</b>';
+
+  emoji_cat_title_helper.appendChild(emoji_cat_title);
+  emoji_scroll[i].appendChild(emoji_cat_title_helper);
+ 
+   var emoji_smiles_row = document.createElement("div");
+  emoji_smiles_row.setAttribute("class", "emoji_smiles_row");
+
+   for(var j=0;j<codes.length;j++){
+    var emeg = document.createElement("a");
+    
+    emeg.setAttribute('class', 'emoji_smile_cont emmegapuck');
+    addEmoji.code=codes[j];
+    emeg.addEventListener('mousedown', addEmoji.bind(undefined, codes[j], emeg) ,true);
+    emeg.addEventListener('mouseover', emojiOver.bind(undefined, emeg) ,true);
+    emeg.addEventListener('mouseout', emojiOut.bind(undefined, emeg) ,true);
+
+    var emoji_bg = document.createElement("div");
+    emoji_bg.setAttribute("class", "emoji_bg");
+
+    var emoji_shadow = document.createElement("div");
+    emoji_shadow.setAttribute("class", "emoji_bg");
+
+    var imgemj = document.createElement("img");
+
+    imgemj.setAttribute("class", "emoji");
+    imgemj.setAttribute("src", '/images/emoji/'+codes[j]+'.png');
+
+    emeg.appendChild(emoji_bg);
+    emeg.appendChild(emoji_shadow);
+    emeg.appendChild(imgemj);
+
+
+    emoji_smiles_row.appendChild(emeg);
+
+    //emoji_scroll[i].appendChild(emeg); 
+    }
+    emoji_scroll[i].appendChild(emoji_smiles_row);
+  }
+  for (var i = 0; i < x.length; i++) {
+    x[i].ondblclick = EmojiDelete;
+  }
+
+
+}
+
+function EmojiDelete(){
+   // on_add=false;
+    var emmegapuck = document.getElementsByClassName("emmegapuck");
+     var count=emmegapuck.length;
+     for (var i = 0; i < count; i++) {
+        emmegapuck[0].parentNode.removeChild(emmegapuck[0]);
+     }
+     for (var i = 0; i < x.length; i++) {
+        x[i].ondblclick = EmojiAddList;
+     }
+}
+
+function focusInput(node){
+  
+  var childsN=parentNodeNo(node,14).children;
+  for (var i = 0; i < childsN.length; i++) {
+    if(childsN[i].getAttribute('contenteditable')){
+      childsN[i].focus();
+      break;
+    }
+    else{
+      var childsN1=childsN[i].children;
+      for (var j = 0; j < childsN1.length; j++) {
+        if(childsN1[j].getAttribute('contenteditable')){
+          childsN1[j].focus();
+          break;
+        }
+      }
+    }
+  };
+}
+
+function parentNodeNo(node,count){
+  if(count--){
+    node1=node.parentNode;
+    parentNodeNo(node1,count);
+  }
+  return node1;
+}
+
+function emojiOver(obj){
+  obj.classList.add('emoji_over');
+}
+
+function emojiOut(obj){
+  obj.classList.remove('emoji_over');
+}
+
+function addEmoji(code, node){
+  focusInput(node);
+  var html=getEmojiHTML(code,codeToChr(code));
+  insertHTML(html);
+}
+
+function codeToChr(code){
+  var len = Math.round(code.length / 4);
+  var chr = '';
+  var i = 0;
+  while(len--) {
+    chr += String.fromCharCode(parseInt(code.substr(i, 4), 16))
+    i += 4;
+  }
+  return chr;
+}
+
+function insertHTML(html){
+  if (html) {
+    document.execCommand('insertHTML', false, html);
+  }
+}
+
+function getEmojiHTML(code,symbol){
+  return '<img class="emoji" '+(symbol ? 'alt="'+symbol+'"' : '')+' src="/images/emoji/'+code+'.png" />';
+}
